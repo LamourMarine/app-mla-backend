7d952e372f1a8c1fc9d15ff0cadf442256cd6866
Marine Lamour <marine.lamour@gmx.fr>
2025-10-30 10:28:18 +0100

    fix: ensure JWT Authorization header is read in Symfony

---
+++ src/EventListener/JwtAuthorizationListener.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\EventListener;
+
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class JwtAuthorizationListener
+{
+    public function onKernelRequest(RequestEvent $event)
+    {
+        $request = $event->getRequest();
+
+        if (!$request->headers->has('Authorization')) {
+            if (!empty($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+                $request->headers->set('Authorization', $_SERVER['REDIRECT_HTTP_AUTHORIZATION']);
+            }
+        }
+    }
+}
